b72e655c20d35d8051778010150d30aef4cca4b5
Sabine Jensen <ssje71367@edu.ucl.dk>
2026-02-02 11:47:08 +0100

    Initial commit(local)

---
+++ Application/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace Application
+{
+    public class Car(string Make, string Model, int Year)
+    {
+		private string? make;
+		private string? model;
+		private int year;
+		private int odometer;
+		private bool isArchived;
+
+
+
+	}
+}
