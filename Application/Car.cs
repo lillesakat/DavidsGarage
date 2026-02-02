c225d33039a1b833b8d1917d21d869039af3ebfd
Sabine Jensen <ssje71367@edu.ucl.dk>
2026-02-02 12:01:43 +0100

    Add part class

--- Application/Car.cs
+++ Application/Car.cs
@@ -2,11 +2,11 @@
 {
     public class Car(string Make, string Model, int Year)
     {
-		private string? make;
-		private string? model;
-		private int year;
-		private int odometer;
-		private bool isArchived;
+		private string? make { get; set; }
+		private string? model { get; set; }
+		private int year { get; set; }
+		private int odometer { get; set; }
+		private bool isArchived { get; set; }
 
 
 
